1b15d802aa4da1f411370191564a1ed7cc54acad
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-24 11:45:55 +0530

    profile controller create edite and update method

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Session;
 use Illuminate\Http\Request;
 use App\User;
+use Auth;
 
 class ProfilesController extends Controller
 {
@@ -12,5 +14,28 @@ class ProfilesController extends Controller
         $username = User::where('username', $username)->first();
 
         return view('profile.profile')->with('user', $username);
+    }
+
+    public function edit()
+    {
+        return view('profile.edit')->with('info', Auth::user()->profile);
+    }
+
+    public function update(Request $request)
+    {
+        //dd($request->all());
+
+        $this->validate($request, [
+            'location' => 'required|max:255',
+            'about' => 'required'
+        ]);
+
+        Auth::user()->profile()->update([
+            'location' => $request->location,
+            'about' => $request->about
+        ]);
+
+        Session::flash('success', 'Profile Updated');
+        return redirect()->back();
     }
 }
