12496f8a2b552e305b65da2846524e086c1237ef
kratos619 <gaurav.palaspagar@hotmail.com>
2018-11-29 16:59:17 +0530

    update

--- app/User.php
+++ app/User.php
@@ -5,12 +5,12 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use App\Traits\Friendable;
+//use App\Traits\Friendable;
 
 class User extends Authenticatable
 {
     use Notifiable;
-    use Friendable;
+  //  use Friendable;
 
     /**
      * The attributes that are mass assignable.
